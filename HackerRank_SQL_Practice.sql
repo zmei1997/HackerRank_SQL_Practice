ecfcdcfca19740c101903584e69ef3d6b8f63015
zmei1997 <68665894+zmei1997@users.noreply.github.com>
2021-02-19 18:58:14 -0500

    [Updating] HackerRank_SQL

---
+++ HackerRank_SQL_Practice.sql
@@ -0,0 +1,34 @@
+/**
+Advanced Select - Type of Triangle
+**/
+-- Solution:
+-- SQL Server
+select  case 
+            when A + B > C AND A + C > B AND B + C > A THEN
+                CASE when A = B and B = C THEN 'Equilateral'
+                     when A = B or A = C or B = C then 'Isosceles'
+                else 'Scalene'
+                end
+        else 'Not A Triangle'
+        end
+from TRIANGLES;
+
+/**
+Advanced Select - The PADS
+**/
+-- Solution:
+-- SQL Server
+select  name + '(' + left(occupation, 1) + ')'
+from    OCCUPATIONS
+order by    name asc;
+
+select  'There are a total of ' + cast(count(occupation) as varchar(10)) + ' ' + lower(occupation) + 's.'
+from    OCCUPATIONS
+group by    occupation
+order by    count(occupation), occupation asc;
+
+/**
+Advanced Select - Occupations - Pivot
+**/
+-- Solution:
+-- SQL Server
