bd1a5909aea9395d6fe7deddcaf53627b891d784
zmei1997 <68665894+zmei1997@users.noreply.github.com>
2021-02-20 16:16:13 -0500

    update solutions for New Companies

--- HackerRank_SQL_Practice.sql
+++ HackerRank_SQL_Practice.sql
@@ -55,3 +55,38 @@ select N,
         end
 from BST
 order by N;
+
+/**
+Advanced Select - New Companies
+**/
+-- Solution:
+-- SQL Server
+-- use WHERE clouse
+select  c.company_code, c.founder, count(distinct l.lead_manager_code), 
+        count(distinct s.senior_manager_code), count(distinct m.manager_code), count(distinct e.employee_code)
+from    Company c, Lead_Manager l, Senior_Manager s, Manager m, Employee e
+where   c.company_code = l.company_code and
+        l.lead_manager_code = s.lead_manager_code and
+        s.senior_manager_code = m.senior_manager_code and
+        m.manager_code = e.manager_code
+group by    c.company_code, c.founder
+order by    c.company_code asc;
+
+-- use JOIN
+select  c.company_code, c.founder, count(distinct l.lead_manager_code), 
+        count(distinct s.senior_manager_code), count(distinct m.manager_code), count(distinct e.employee_code)
+from    Company c
+        inner join
+        Lead_Manager l
+        on c.company_code = l.company_code
+        inner join
+        Senior_Manager s
+        on l.lead_manager_code = s.lead_manager_code
+        inner join
+        Manager m
+        on s.senior_manager_code = m.senior_manager_code
+        inner join
+        Employee e
+        on m.manager_code = e.manager_code
+group by    c.company_code, c.founder
+order by    c.company_code asc;
