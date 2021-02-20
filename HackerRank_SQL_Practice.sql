e8627489b622851f2dc98d08c2d4be8e852460fd
zmei1997 <68665894+zmei1997@users.noreply.github.com>
2021-02-20 14:52:14 -0500

    update solutions for Advanced Select BTS

--- HackerRank_SQL_Practice.sql
+++ HackerRank_SQL_Practice.sql
@@ -42,3 +42,16 @@ PIVOT
 (
     MAX(NAME) FOR Occupation IN ([Doctor], [Professor], [Singer], [Actor])
 ) AS pivotTable
+
+/**
+Advanced Select - Binary Tree Nodes
+**/
+-- Solution:
+-- SQL Server
+select N,
+        case when P is null then 'Root'
+             when N in (select P from BST) then 'Inner'
+        else 'Leaf'
+        end
+from BST
+order by N;
